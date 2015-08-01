8845b95fcfe4c689c9048c6916af01b296ad2c83
Zaicheng Qi <vmlinz@gmail.com>
2015-08-01 16:47:30 +0800

    Solve more koans, getting closer to enlightenment

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -18,7 +18,12 @@
 #
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    if a == b and a == c:
+    	return 'equilateral'
+    elif a != b and a != c and b != c:
+    	return 'scalene'
+    else:
+    	return 'isosceles'
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
